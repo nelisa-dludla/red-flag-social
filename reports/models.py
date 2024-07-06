88511754b8b123774d80134328114fd59fa6e934
Nelisa Dludla <nelisa.dludla@outlook.com>
2024-07-06 05:00:56 +0200

    Added register template and implemented user registration

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Report(models.Model):
+    # Social Media Platform
+    social_media_platform = models.CharField(max_length=50)
+    # Social Media Handle
+    social_media_handle = models.CharField(max_length=150)
+    # Category
+    category = models.CharField(max_length=30)
+    # Description
+    description = models.TextField()
+    # Created At
+    created_at =  models.DateTimeField(auto_now_add=True)
+    # Reported By
+    reported_by = models.ForeignKey(User, on_delete=models.CASCADE)
