fff75443e5210ce3d45598c33ec488ded367e651
Nelisa Dludla <nelisa.dludla@outlook.com>
2024-07-06 23:19:39 +0200

    Implemented profile page - Allow users to update username or email

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm
 
 def register(request):
     if request.method == 'POST':
@@ -24,7 +24,17 @@ def register(request):
 
 @login_required
 def profile(request):
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        if user_form.is_valid():
+            user_form.save()
+            messages.success(request, 'Your account has been updated!')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+
     context = {
-        'title': 'Profile'
+        'title': 'Profile',
+        'user_form': user_form
     }
     return render(request, 'users/profile.html', context=context)
